a42bb213b9b06e94fc32ca690d8ad55467d90cd0
c0d3engr <c0d3engr@gmail.com>
2025-05-21 03:35:56 +0800

    Initial commit

---
+++ src/test/java/com/c0d3engr/game_currency_tracker/GameCurrencyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c0d3engr.game_currency_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCurrencyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
