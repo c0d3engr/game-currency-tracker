44041cc03534f5066a7a38e574ce835d642c497c
c0d3engr <c0d3engr@gmail.com>
2025-05-30 02:18:38 +0800

    Setup dependency injection for user and account classes

---
+++ src/main/java/com/c0d3engr/game_currency_tracker/model/Account.java
@@ -0,0 +1,12 @@
+package com.c0d3engr.game_currency_tracker.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Account {
+
+    public void addAccount()
+    {
+        System.out.println("Account successfully added!");
+    }
+}
--- src/main/java/com/c0d3engr/game_currency_tracker/model/User.java
+++ src/main/java/com/c0d3engr/game_currency_tracker/model/User.java
@@ -1,12 +1,20 @@
 package com.c0d3engr.game_currency_tracker.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
 
+    @Autowired
+    Account account;
+
     public void addUser()
     {
+
+        account.addAccount();
+        // addCharacter();
+
         System.out.println("User succcessfully added!");
     }
 }
